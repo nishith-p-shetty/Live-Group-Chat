daa796a4e870fe1e2e6bd312a6058cbc78d37478
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-09-12 12:37:56 +0530

    Update client.js

--- static/js/client.js
+++ static/js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://ge-chat.herokuapp.com/');
 
 // Get DOM elements in respective Js variables
 const form = document.getElementById('send-container');
